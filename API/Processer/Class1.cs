2ac49fc07d98ac7ee417de9dfcc6cab234ec67bb
Marcelo <marcelo.analistadesistemas@gmail.com>
2019-07-12 13:59:22 -0300

    ajustments

---
+++ API/Processer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Processer
+{
+    public class Class1
+    {
+    }
+}
--- Client/Singular/Client/Program.cs
+++ Client/Singular/Client/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
-
+﻿
 namespace Client
 {
-    class Program
+    using DAL;
+    using Microsoft.Win32;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+              
+                List<string> list = new ClientDAL().InstalledsPrograms();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+               
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
     }
 }
