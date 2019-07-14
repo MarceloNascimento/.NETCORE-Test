dd21083b5a358d3dd9449c8ebe2d432d0867e18d
Marcelo <marcelo.analistadesistemas@gmail.com>
2019-07-13 23:03:08 -0300

    adjusments

--- Database Scripts/DDL.sql
+++ Database Scripts/DDL.sql
@@ -29,3 +29,12 @@ CREATE TABLE Programs (
     REFERENCES Machines(id_machine)      
 );
 
+--------------------------- DML basics running system ------------------------
+INSERT Machines (ds_name, dt_datehours)  VALUES ('mac-xpto-172', '2019-07-13 22:09:22.963');
+INSERT Machines (ds_name, dt_datehours)  VALUES ('mac-xpto-173', '2019-07-13 22:09:22.963');
+
+INSERT Programs (ds_name, machine_id)  VALUES ('7 zip', 1);
+INSERT Programs (ds_name, machine_id)  VALUES ('oracle', 1);
+INSERT Programs (ds_name, machine_id)  VALUES ('mongodb', 2);
+
+INSERT Users (ds_fullname, ds_login,ds_password)  VALUES ('Sr. Godofredo Vacario', 'Admin', HashBytes('MD5', 'Test'));
